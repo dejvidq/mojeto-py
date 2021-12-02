bdfc5017317b171d47b604678f178a936965279c
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-02 23:08:37 +0100

    Added return types

--- src/mojeto/cli/add.py
+++ src/mojeto/cli/add.py
@@ -5,10 +5,10 @@ from mojeto.config import Config
 
 class Add:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.config = Config()
 
-    def __call__(self, file_path):
+    def __call__(self, file_path) -> None:
         p = Path(file_path)
         name = p.name
         path = str(p.parents[0])
--- src/mojeto/config.py
+++ src/mojeto/config.py
@@ -11,17 +11,17 @@ class Config:
         if self.config[1]['files'] is None:
             self.config[1]['files'] = {}
 
-    def open_config(self):
+    def open_config(self) -> list:
         with open(self.config_path, "r") as conf_file:
             yaml_conf = yaml.safe_load(conf_file.read())
         return yaml_conf
 
-    def add_file(self, file_name, file_path):
+    def add_file(self, file_name, file_path) -> None:
         self.config[1]['files'][file_name] = {
             "path": file_path
         }
         self.write_config()
 
-    def write_config(self):
+    def write_config(self) -> None:
         with open(self.config_path, "w") as conf_file:
             conf_file.write(yaml.safe_dump(self.config))
