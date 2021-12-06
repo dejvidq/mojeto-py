f9bc8a777709b67136244a813244e906433f4061
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-06 11:26:31 +0100

    small refactor for flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,21 @@ import string
 
 import pytest
 
-from mojeto.cli.init import Init
+from src.mojeto.cli.init import Init
+
 
 @pytest.fixture
 def random_repo_name():
     repo_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
     return f"/tmp/{repo_name}"
 
+
 @pytest.fixture
 def generate_random_repo(random_repo_name):
     mojeto_init = Init(random_repo_name)
     mojeto_init.create_working_directory()
     return mojeto_init.repo_location
+
 
 @pytest.fixture
 def generate_random_repo_with_config(random_repo_name):
