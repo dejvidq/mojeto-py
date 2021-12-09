90b42faa7f97eb9c6b879672db2dfbed3d22b0c2
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-09 14:26:50 +0100

    Added 'test' session to nox

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import nox
 
 
@@ -5,3 +8,15 @@ import nox
 def lint(session):
     session.install("flake8", "flake8-import-order")
     session.run("flake8")
+
+
+@nox.session
+def test(session):
+    sdist_dir = os.path.join(session.virtualenv.location, "sdist")
+    if os.path.exists(sdist_dir):
+        shutil.rmtree(sdist_dir, ignore_errors=True)
+    session.run("python", "setup.py", "sdist", "--dist-dir", sdist_dir, silent=True,)
+    generated_files = os.listdir(sdist_dir)
+    assert len(generated_files) == 1
+    generated_sdist = os.path.join(sdist_dir, generated_files[0])
+    session.install(generated_sdist)
