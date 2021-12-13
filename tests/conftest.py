e87eb60b4af97ea911a046dd488744c31604abd1
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-13 15:36:09 +0100

    test init works

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import os
+from pathlib import Path
 import random
+import shutil
 import string
 
 import pytest
@@ -10,3 +12,14 @@ def repo_location():
     default_location = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
     location = os.environ.get("MOJETO_CONFIG_LOCATION", f"/tmp/{default_location}")
     yield location
+    shutil.rmtree(path=location, ignore_errors=True)
+
+
+@pytest.fixture
+def repo_location_created():
+    default_location = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    location = os.environ.get("MOJETO_CONFIG_LOCATION", f"/tmp/{default_location}")
+    path = Path(location)
+    path.mkdir(parents=True, exist_ok=True)
+    yield location
+    shutil.rmtree(path=location, ignore_errors=True)
