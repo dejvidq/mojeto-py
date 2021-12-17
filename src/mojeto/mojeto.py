7b9ee9fb2f81da11d730da32a1d2dd229f1cfe25
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-17 15:05:05 +0100

    Start working on apply

--- src/mojeto/mojeto.py
+++ src/mojeto/mojeto.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
 from mojeto.cli.add import Add
+from mojeto.cli.apply import Apply
 from mojeto.cli.backup import Backup
 from mojeto.cli.init import Init
 
@@ -8,9 +9,10 @@ from mojeto.cli.init import Init
 def main() -> None:
     parser = ArgumentParser(description="Take care of your dotfiles")
     exclusive = parser.add_mutually_exclusive_group()
-    exclusive.add_argument('-b', '--backup', help="Do backup", action='store_true')
     exclusive.add_argument('-a', '--add', help="Add file")
+    exclusive.add_argument('-b', '--backup', help="Do backup", action='store_true')
     exclusive.add_argument('-i', '--init', help="Initialize configuration", nargs="?", const="")
+    exclusive.add_argument('-p', '--apply', help="Apply files from repository to system", action="store_true")
 
     args = parser.parse_args()
 
@@ -23,3 +25,6 @@ def main() -> None:
     elif args.backup:
         backup = Backup()
         backup()
+    elif args.apply:
+        apply = Apply()
+        apply()
