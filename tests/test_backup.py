73230e3731b609206f16ca67bd369024756172ce
dejvidq <15433565+dejvidq@users.noreply.github.com>
2021-12-17 14:09:13 +0100

    Backup is checking if file exist before trying to copy it

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,3 +1,5 @@
+import os
+
 from mojeto.cli.backup import Backup
 
 
@@ -12,6 +14,14 @@ class TestBackup:
         content_after = self.read_file_content(file)
         assert not content_before == content_after
 
+    def test_backup_file_does_not_exist(self, init_with_file, capfd):
+        _, file = init_with_file
+        self.remove_file(file)
+        backup = Backup()
+        backup()
+        out, _ = capfd.readouterr()
+        assert out.strip() == f"File {file} does not exist. Omitting"
+
     def read_file_content(self, file):
         with open(file, "r") as f:
             return f.read()
@@ -19,3 +29,6 @@ class TestBackup:
     def override_file_content(self, file):
         with open(file, "w+") as f:
             f.write("AFTER")
+
+    def remove_file(self, filename):
+        os.remove(filename)
